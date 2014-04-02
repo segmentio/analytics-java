2b7534621345df5830146d78fa04460568e2f980
Joshua Chaitin-Pollak <josh@offthehill.org>
2014-04-02 19:05:47 -0400

    use commons-lang3

--- src/main/java/com/github/segmentio/models/Props.java
+++ src/main/java/com/github/segmentio/models/Props.java
@@ -6,7 +6,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.commons.lang.ClassUtils;
+import org.apache.commons.lang3.ClassUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
