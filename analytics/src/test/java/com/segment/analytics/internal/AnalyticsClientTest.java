5a05e63f62b3362358c4ea3defb235bc0901718b
Prateek Srivastava <f2prateek@gmail.com>
2015-10-10 16:51:57 -0600

    Add a limit to number of retries
    Also refactors some of the AnalyticsClient code and improves test
    documentation.

--- analytics/src/test/java/com/segment/analytics/internal/AnalyticsClientTest.java
+++ analytics/src/test/java/com/segment/analytics/internal/AnalyticsClientTest.java
@@ -11,6 +11,7 @@ import com.segment.backo.Backo;
 import com.squareup.burst.BurstJUnit4;
 import java.io.IOException;
 import java.util.Collections;
+import java.util.Queue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -22,7 +23,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import retrofit.RetrofitError;
 import retrofit.converter.ConversionException;
 
@@ -30,40 +30,46 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
-@RunWith(BurstJUnit4.class) public class AnalyticsClientTest {
-  AnalyticsClient client;
+@RunWith(BurstJUnit4.class) //
+public class AnalyticsClientTest {
+  // Backo instance for testing which trims down the wait times.
+  private static final Backo BACKO =
+      Backo.builder().base(TimeUnit.NANOSECONDS, 1).factor(1).build();
+
+  Log log = Log.NONE;
+  ThreadFactory threadFactory;
   @Mock BlockingQueue<Message> messageQueue;
   @Mock SegmentService segmentService;
-  @Mock Log log;
-  @Mock ThreadFactory threadFactory;
   @Mock ExecutorService networkExecutor;
-  @Mock Thread thread;
 
   @Before public void setUp() {
     initMocks(this);
+    threadFactory = Executors.defaultThreadFactory();
+  }
 
-    when(threadFactory.newThread(any(Runnable.class))).thenReturn(thread);
+  // Defers loading the client until tests can initialize all required dependencies.
+  AnalyticsClient newClient() {
+    return new AnalyticsClient(messageQueue, segmentService, 50, TimeUnit.HOURS.toMillis(1), log,
+        threadFactory, networkExecutor);
   }
 
   @Test public void enqueueAddsToQueue(MessageBuilderTest builder) {
-    client = new AnalyticsClient(messageQueue, segmentService, 50, TimeUnit.HOURS.toMillis(1), log,
-        threadFactory, networkExecutor);
+    AnalyticsClient client = newClient();
 
     Message message = builder.get().userId("prateek").build();
-
     client.enqueue(message);
 
     verify(messageQueue).add(message);
   }
 
   @Test public void shutdown() {
-    client = new AnalyticsClient(messageQueue, segmentService, 50, TimeUnit.HOURS.toMillis(1), log,
-        threadFactory, networkExecutor);
+    AnalyticsClient client = newClient();
 
     client.shutdown();
 
@@ -72,85 +78,84 @@ import static org.mockito.MockitoAnnotations.initMocks;
   }
 
   @Test public void flushInsertsPoison() {
-    client = new AnalyticsClient(messageQueue, segmentService, 50, TimeUnit.HOURS.toMillis(1), log,
-        threadFactory, networkExecutor);
+    AnalyticsClient client = newClient();
 
     client.flush();
 
     verify(messageQueue).add(FlushMessage.POISON);
   }
 
+  /** Wait until the queue is drained. */
+  static void wait(Queue<?> queue) {
+    //noinspection StatementWithEmptyBody
+    while (queue.size() > 0) {
+    }
+  }
+
+  /**
+   * Verify that a {@link BatchUploadTask} was submitted to the executor, and return the {@link
+   * BatchUploadTask#batch} it was uploading..
+   */
+  static Batch captureBatch(ExecutorService executor) {
+    final ArgumentCaptor<Runnable> runnableArgumentCaptor = ArgumentCaptor.forClass(Runnable.class);
+    verify(executor, timeout(1000)).submit(runnableArgumentCaptor.capture());
+    final BatchUploadTask task = (BatchUploadTask) runnableArgumentCaptor.getValue();
+    return task.batch;
+  }
+
   @Test public void flushSubmitsToExecutor() {
-    threadFactory = Executors.defaultThreadFactory();
     messageQueue = new LinkedBlockingQueue<>();
-    client = new AnalyticsClient(messageQueue, segmentService, 50, TimeUnit.HOURS.toMillis(1), log,
-        threadFactory, networkExecutor);
+    AnalyticsClient client = newClient();
 
     TrackMessage first = TrackMessage.builder("foo").userId("bar").build();
     TrackMessage second = TrackMessage.builder("qaz").userId("qux").build();
     client.enqueue(first);
     client.enqueue(second);
     client.flush();
+    wait(messageQueue);
 
-    //noinspection StatementWithEmptyBody
-    while (messageQueue.size() > 0) {
-      // wait to make sure looper reads the flush message
-    }
-
-    final ArgumentCaptor<Runnable> runnableArgumentCaptor = ArgumentCaptor.forClass(Runnable.class);
-    verify(networkExecutor, Mockito.timeout(1 * 1000)).submit(runnableArgumentCaptor.capture());
-
-    final BatchUploadTask task = (BatchUploadTask) runnableArgumentCaptor.getValue();
-    assertThat(task.batch.batch()).containsExactly(first, second);
+    assertThat(captureBatch(networkExecutor).batch()).containsExactly(first, second);
   }
 
   @Test public void enqueueMaxTriggersFlush() {
-    threadFactory = Executors.defaultThreadFactory();
     messageQueue = new LinkedBlockingQueue<>();
-    client = new AnalyticsClient(messageQueue, segmentService, 5, TimeUnit.HOURS.toMillis(1), log,
-        threadFactory, networkExecutor);
+    AnalyticsClient client = newClient();
 
-    for (int i = 0; i < 5; i++) {
+    // Enqueuing 51 messages (> 50) should trigger flush.
+    for (int i = 0; i < 51; i++) {
       client.enqueue(TrackMessage.builder("Event " + i).userId("bar").build());
     }
+    wait(messageQueue);
 
-    //noinspection StatementWithEmptyBody
-    while (messageQueue.size() > 0) {
-      // wait to make sure looper reads the flush message
-    }
-
-    final ArgumentCaptor<Runnable> runnableArgumentCaptor = ArgumentCaptor.forClass(Runnable.class);
-    verify(networkExecutor, Mockito.timeout(1 * 1000)).submit(runnableArgumentCaptor.capture());
-    final BatchUploadTask task = (BatchUploadTask) runnableArgumentCaptor.getValue();
-    assertThat(task.batch.batch()).hasSize(5);
+    // Verify that the executor saw the batch.
+    assertThat(captureBatch(networkExecutor).batch()).hasSize(50);
   }
 
   @Test public void enqueueBeforeMaxDoesNotTriggerFlush() {
-    threadFactory = Executors.defaultThreadFactory();
     messageQueue = new LinkedBlockingQueue<>();
-    client = new AnalyticsClient(messageQueue, segmentService, 10, TimeUnit.HOURS.toMillis(1), log,
-        threadFactory, networkExecutor);
+    AnalyticsClient client = newClient();
 
+    // Enqueuing 5 messages (< 50) should not trigger flush.
     for (int i = 0; i < 5; i++) {
       client.enqueue(TrackMessage.builder("Event " + i).userId("bar").build());
     }
+    wait(messageQueue);
 
-    //noinspection StatementWithEmptyBody
-    while (messageQueue.size() > 0) {
-      // wait to make sure looper reads the flush message
-    }
-
+    // Verify that the executor didn't see anything.
     verify(networkExecutor, never()).submit(any(Runnable.class));
+  }
+
+  static Batch batchFor(Message message) {
+    return Batch.create(Collections.<String, Object>emptyMap(), Collections.singletonList(message));
   }
 
   @Test public void batchRetriesForNetworkErrors() {
     TrackMessage trackMessage = TrackMessage.builder("foo").userId("bar").build();
-    Batch batch = Batch.create(Collections.<String, Object>emptyMap(),
-        Collections.<Message>singletonList(trackMessage));
-    BatchUploadTask batchUploadTask = new BatchUploadTask(segmentService, batch,
-        Backo.builder().base(TimeUnit.NANOSECONDS, 1).factor(1).build(), log);
-    RetrofitError retrofitError = RetrofitError.networkError(null, new IOException());
+    Batch batch = batchFor(trackMessage);
+    BatchUploadTask batchUploadTask = new BatchUploadTask(segmentService, batch, BACKO, log);
 
+    // Throw a network error 3 times.
+    RetrofitError retrofitError = RetrofitError.networkError(null, new IOException());
     when(segmentService.upload(batch)).thenThrow(retrofitError)
         .thenThrow(retrofitError)
         .thenThrow(retrofitError)
@@ -158,21 +163,36 @@ import static org.mockito.MockitoAnnotations.initMocks;
 
     batchUploadTask.run();
 
+    // Verify that we tried to upload 4 times, 3 failed and 1 succeeded.
     verify(segmentService, times(4)).upload(batch);
   }
 
   @Test public void batchDoesNotRetryForNonNetworkErrors() {
     TrackMessage trackMessage = TrackMessage.builder("foo").userId("bar").build();
-    Batch batch = Batch.create(Collections.<String, Object>emptyMap(),
-        Collections.<Message>singletonList(trackMessage));
-    BatchUploadTask batchUploadTask = new BatchUploadTask(segmentService, batch,
-        Backo.builder().base(TimeUnit.NANOSECONDS, 1).factor(1).build(), log);
+    Batch batch = batchFor(trackMessage);
+
+    BatchUploadTask batchUploadTask = new BatchUploadTask(segmentService, batch, BACKO, log);
     RetrofitError retrofitError =
         RetrofitError.conversionError(null, null, null, null, new ConversionException("fake"));
     doThrow(retrofitError).when(segmentService).upload(batch);
 
     batchUploadTask.run();
 
-    verify(segmentService, times(1)).upload(batch);
+    // Verify we only tried to upload once.
+    verify(segmentService).upload(batch);
+  }
+
+  @Test public void givesUpAfterMaxRetries() {
+    TrackMessage trackMessage = TrackMessage.builder("foo").userId("bar").build();
+    Batch batch = batchFor(trackMessage);
+
+    BatchUploadTask batchUploadTask = new BatchUploadTask(segmentService, batch, BACKO, log);
+    RetrofitError retrofitError = RetrofitError.networkError(null, new IOException());
+    when(segmentService.upload(batch)).thenThrow(retrofitError);
+
+    batchUploadTask.run();
+
+    // 50 == MAX_ATTEMPTS in AnalyticsClient.java
+    verify(segmentService, times(50)).upload(batch);
   }
 }
