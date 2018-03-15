e8b7068cf5736ad39f2ea3d2eedb43531a83cb43
Prateek Srivastava <f2prateek@gmail.com>
2018-03-15 16:38:09 -0700

    Migrate to Circle CI v2
    Our setup doesn't seem to be working with JDK 9 yet (fails with `Fatal error compiling: java.lang.NoClassDefFoundError: javax/annotation/Generated: javax.annotation.Generated`). Leaving it out for now, since the old setup only used JDK 8.
    
    Circle doesn't support wildcard checksums very well, and since we have multiple `pom.xml` files, I've skipped caching dependencies for now.

--- .buildscript/deploy_snapshot.sh
+++ .buildscript/deploy_snapshot.sh
@@ -5,20 +5,20 @@
 # Adapted from https://coderwall.com/p/9b_lfq and
 # http://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/
 
-SLUG="segmentio/analytics-java"
+REPO="analytics-java"
+USERNAME="segmentio"
 JDK="oraclejdk8"
 BRANCH="master"
 
-set -e
-
-if [ "$TRAVIS_REPO_SLUG" != "$SLUG" ]; then
-  echo "Skipping snapshot deployment: wrong repository. Expected '$SLUG' but was '$TRAVIS_REPO_SLUG'."
-elif [ "$TRAVIS_JDK_VERSION" != "$JDK" ]; then
-  echo "Skipping snapshot deployment: wrong JDK. Expected '$JDK' but was '$TRAVIS_JDK_VERSION'."
-elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  echo "Skipping snapshot deployment: was pull request."
-elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
-  echo "Skipping snapshot deployment: wrong branch. Expected '$BRANCH' but was '$TRAVIS_BRANCH'."
+if [ "$CIRCLE_PROJECT_REPONAME" != "$REPO" ]; then
+  echo "Skipping snapshot deployment: wrong repository. Expected '$REPO' but was '$CIRCLE_PROJECT_REPONAME'."
+elif [ "$CIRCLE_PROJECT_USERNAME" != "$USERNAME" ]; then
+  echo "Skipping snapshot deployment: wrong owner. Expected '$USERNAME' but was '$CIRCLE_PROJECT_USERNAME'."
+elif [ "$CIRCLE_JDK_VERSION" != "$JDK" ]; then
+  # $CIRCLE_JDK_VERSION must be manually set in circle.yml
+  echo "Skipping snapshot deployment: wrong JDK. Expected '$JDK' but was '$CIRCLE_JDK_VERSION'."
+elif [ "$CIRCLE_BRANCH" != "$BRANCH" ]; then
+  echo "Skipping snapshot deployment: wrong branch. Expected '$BRANCH' but was '$CIRCLE_BRANCH'."
 else
   echo "Deploying snapshot..."
   mvn clean source:jar javadoc:jar deploy --settings=".buildscript/settings.xml" -Dmaven.test.skip=true
