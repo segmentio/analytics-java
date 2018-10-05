21bf215df46acacb9704b1d41ebd72bd97cd28d7
Nhi Nguyen <nhi.nguyen@segment.com>
2018-10-05 14:36:12 -0700

    Use the latest version of the e2e test harness

--- .buildscript/e2e.sh
+++ .buildscript/e2e.sh
@@ -6,8 +6,10 @@ if [ "$RUN_E2E_TESTS" != "true" ]; then
   echo "Skipping end to end tests."
 else
   echo "Running end to end tests..."
-  wget https://github.com/segmentio/library-e2e-tester/releases/download/0.2.1/tester_linux_amd64
-  chmod +x tester_linux_amd64
-  ./tester_linux_amd64 -segment-write-key="$SEGMENT_WRITE_KEY" -webhook-auth-username="$WEBHOOK_AUTH_USERNAME" -webhook-bucket="$WEBHOOK_BUCKET" -path='.buildscript/cli.sh'
+  exe="tester_linux_amd64"
+  rm -f ${exe}
+  wget -O - https://raw.githubusercontent.com/segmentio/library-e2e-tester/master/.buildscript/get-latest-version.sh | bash -s ${exe}
+  chmod +x ${exe}
+  ./${exe} -segment-write-key="$SEGMENT_WRITE_KEY" -webhook-auth-username="$WEBHOOK_AUTH_USERNAME" -webhook-bucket="$WEBHOOK_BUCKET" -path='.buildscript/cli.sh'
   echo "End to end tests completed!"
 fi
