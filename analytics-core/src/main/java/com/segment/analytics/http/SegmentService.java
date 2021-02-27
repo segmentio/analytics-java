80a53bca781e385a10902e705b99ef3713c9e483
Patrick Bassut <patrickbassut@hotmail.com>
2021-02-27 04:13:11 -0300

    cant put dot in Retrofits service

--- analytics-core/src/main/java/com/segment/analytics/http/SegmentService.java
+++ analytics-core/src/main/java/com/segment/analytics/http/SegmentService.java
@@ -7,6 +7,6 @@ import retrofit2.http.POST;
 
 /** REST interface for the Segment API. */
 public interface SegmentService {
-  @POST("")
+  @POST(".")
   Call<UploadResponse> upload(@Body Batch batch);
 }
