db7d2a2dcb2ddf7d72e6cc65b07272071013be9e
Hunter Medney <hunter_medney@us.ibm.com>
2021-03-29 22:33:30 -0400

    add userIdOrAnonymousIdIsRequired test

--- analytics-core/src/test/java/com/segment/analytics/messages/IdentifyMessageTest.java
+++ analytics-core/src/test/java/com/segment/analytics/messages/IdentifyMessageTest.java
@@ -1,6 +1,7 @@
 package com.segment.analytics.messages;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
 
 import com.google.common.collect.ImmutableMap;
 import org.junit.Test;
@@ -22,6 +23,32 @@ public class IdentifyMessageTest {
       IdentifyMessage.builder().userId("userId").build();
     } catch (IllegalStateException e) {
       assertThat(e).hasMessage("Either userId or traits must be provided.");
+    }
+  }
+
+  @Test
+  public void userIdOrAnonymousIdIsRequired() {
+    final String exceptionMessage = "Either anonymousId or userId must be provided.";
+
+    try {
+      IdentifyMessage.builder().build();
+      fail();
+    } catch (IllegalArgumentException e) {
+      assertThat(e).hasMessage(exceptionMessage);
+    }
+
+    try {
+      IdentifyMessage.builder().userId(null).anonymousId((String) null).build();
+      fail();
+    } catch (IllegalArgumentException e) {
+      assertThat(e).hasMessage(exceptionMessage);
+    }
+
+    try {
+      IdentifyMessage.builder().userId("").anonymousId("").build();
+      fail();
+    } catch (IllegalArgumentException e) {
+      assertThat(e).hasMessage(exceptionMessage);
     }
   }
 
