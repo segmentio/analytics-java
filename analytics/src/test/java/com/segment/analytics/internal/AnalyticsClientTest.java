76550a044d16bf93e9451ba0e73eec7db4bbd362
jorgen25 <jorge@team.northtwofive.com>
2022-01-26 13:22:57 -0700

    run mvn:spotless apply

--- analytics/src/test/java/com/segment/analytics/internal/AnalyticsClientTest.java
+++ analytics/src/test/java/com/segment/analytics/internal/AnalyticsClientTest.java
@@ -4,15 +4,15 @@ import static com.segment.analytics.internal.FlushMessage.POISON;
 import static com.segment.analytics.internal.StopMessage.STOP;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
