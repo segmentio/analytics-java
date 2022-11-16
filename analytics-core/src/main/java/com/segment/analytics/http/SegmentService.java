2f89e139fdbcef707e058b32c6e7ea4a62098d1f
David Menear <davidlmenear@gmail.com>
2022-11-16 07:37:13 -0800

    Call upload URL using Retrofit's @Url annotation (#391)
    * call upload URL using Retrofit's @Url annotation
    
    * fix formatting
    
    Co-authored-by: David Menear <dmenear@users.noreply.github.com>

--- analytics-core/src/main/java/com/segment/analytics/http/SegmentService.java
+++ analytics-core/src/main/java/com/segment/analytics/http/SegmentService.java
@@ -1,12 +1,14 @@
 package com.segment.analytics.http;
 
 import com.segment.analytics.messages.Batch;
+import okhttp3.HttpUrl;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
+import retrofit2.http.Url;
 
 /** REST interface for the Segment API. */
 public interface SegmentService {
-  @POST(".")
-  Call<UploadResponse> upload(@Body Batch batch);
+  @POST
+  Call<UploadResponse> upload(@Url HttpUrl uploadUrl, @Body Batch batch);
 }
