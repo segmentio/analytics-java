5a18f2ff7ec02799b19cee4f7fbdd4a963d02567
neelkanth-kaushik <nkaushik@segment.com>
2025-11-17 15:17:53 +0530

    Patch for Github issue #524

--- analytics/src/main/java/com/segment/analytics/internal/AnalyticsClient.java
+++ analytics/src/main/java/com/segment/analytics/internal/AnalyticsClient.java
@@ -25,7 +25,7 @@ import java.util.UUID;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.Future; 
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.ScheduledExecutorService;
